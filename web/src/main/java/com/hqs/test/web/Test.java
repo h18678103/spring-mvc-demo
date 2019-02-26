6fbcc61d3bb97e6b573090f40f767bc61c930b44
胡勤松 <18678103@qq.com>
2019-02-26 16:12:24 +0800

    2

---
+++ web/src/main/java/com/hqs/test/web/Test.java
@@ -0,0 +1,12 @@
+package com.hqs.test.web;
+
+/**
+ * @author huqinsong
+ * @date 2019/2/26
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(1);
+        System.out.println(2);
+    }
+}
