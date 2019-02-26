052d14135488cd77819447b3af586aa8fea0b607
胡勤松 <18678103@qq.com>
2019-02-26 10:13:45 +0800

    2019-02-26git_test_2

---
+++ util/src/test/java/Txeser.java
@@ -0,0 +1,14 @@
+/**
+ * @author huqinsong
+ * @date 2018/10/15
+ */
+public class Txeser {
+    public static void main(String[] args) {
+        Boolean b = false;
+        if (b !=null && !b){
+            System.out.println(1);
+        }else {
+            System.out.println(2);
+        }
+    }
+}
